5bc2756c1f1c77705e8f2c7b73cd0c36c8817c74
GABRIEL SANTOS DE GODOY <gabriel.sgodoy1@senacsp.edu.br>
2024-11-29 22:04:40 -0300

    Aula

--- app.js
+++ app.js
@@ -9,6 +9,29 @@ app.get('/', (requisicao, resposta) => {
     resposta.status(200).send(carros2024);
 });
 
+app.get('/:sigla', (requisicao, resposta) => {
+    const siglaInformada = requisicao.params.sigla.toUpperCase(); // Obtendo a Sigla
+    const carro = carros2024.find((infoCarro) => infoCarro.sigla === siglaInformada); // Busca o carro pela sigla
+    if (!carro) {
+        // ! Vazio Not
+        // Se o carro não for enctrado, retorna erro 404
+        resposta
+        .status(404)
+        .send(
+            'Não existe um carro com a sigla informada!' //Mensagem de erro
+        );
+    return;
+    }
+    resposta.status(200).send(carro); // Se encontrado a sigla retorna a resposta correta
+});
+
+app.post('/', (req, res) => {
+    const novoCarro = req.body; // Obtém o corpo enviado para incluir um carro
+    carros2024.push(novoCarro); // Adiciona o novo carro à lista de carros.
+    res.status(200).send(novoCarro); // Retorna o carro adicionado com status 200 (Ok).
+});
+
+
 // Inicia o servidor na porta 3000:
 app.listen(3000,() => console.log("Servidor Rodando com Sucesso"));
 
--- tabelacarros.js
+++ tabelacarros.js
@@ -17,6 +17,42 @@ const carros2024 = [
         aceleracao: 2.8,
         preco: 330000,
     },
+    {
+        nome: 'BMW',
+        sigla:'BMW',
+        velocidadeMaxima: 200,
+        potencia: 500,
+        consumo: 7.5,
+        aceleracao: 4.0,
+        preco: 120000,
+    },
+    {
+        nome: 'Audi',
+        sigla:'AUD',
+        velocidadeMaxima: 290,
+        potencia: 650,
+        consumo: 7.0,
+        aceleracao: 3.6,
+        preco: 11500,
+    },
+    {
+        nome: 'Honda',
+        sigla:'HON',
+        velocidadeMaxima: 220,
+        potencia: 200,
+        consumo: 12.0,
+        aceleracao: 6.5,
+        preco: 35000,
+    },
+    {
+        nome: 'Ford',
+        sigla:'FOR',
+        velocidadeMaxima: 230,
+        potencia: 250,
+        consumo: 11.0,
+        aceleracao: 6.0,
+        preco: 40000,
+    },
 ];
 
 export default carros2024;
