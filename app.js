7bf0e4f260641c376857880e501348cd3a58075d
GABRIEL SANTOS DE GODOY <gabriel.sgodoy1@senacsp.edu.br>
2024-12-06 22:19:17 -0300

    Aula

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import carros2024 from './tabelacarros.js';
 import express from 'express';
 
+import { modeloCarro, modeloAtualizacaoCarro } from './validacao.js';
+
 const app = express();
 
 app.use(express.json());
@@ -27,10 +29,16 @@ app.get('/:sigla', (requisicao, resposta) => {
 
 app.post('/', (req, res) => {
     const novoCarro = req.body; // Obtém o corpo enviado para incluir um carro
+    // JOI
+    const { error } = modeloCarro.validate(novoCarro);
+    if (error) {
+        // Se houver erro retorna erro 400 (Bad Request).
+        res.status(400).send(error);
+        return;
+    }
     carros2024.push(novoCarro); // Adiciona o novo carro à lista de carros.
-    res.status(200).send(novoCarro); // Retorna o carro adicionado com status 200 (Ok).
+    res.status(201).send(novoCarro); // Retorna o carro adicionado com status 201 (Ok).
 });
-
 
 app.put('/:sigla', (req, res) => {
     const siglaInformada = req.params.sigla.toUpperCase(); // Obtem a Sigla
@@ -43,6 +51,13 @@ app.put('/:sigla', (req, res) => {
         );
         return;
     };
+    // JOI
+    const { error } = modeloAtualizacaoCarro.validate(req.body);
+    if (error) {
+        // Se houver erro no modelo/validação retorna erro
+        res.status(400).send(error);
+        return;
+    }
     const campos = Object.keys(req.body); // Obtém o corpo da requisição enviada
     for (let campo of campos) {
         carroSelecionado[campo] = req.body[campo]; // Atualiza o carro com a informação
