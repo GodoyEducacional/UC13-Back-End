e35e311642848089445d9922c4ee99e118f2beaf
GABRIEL SANTOS DE GODOY <gabriel.sgodoy1@senacsp.edu.br>
2025-01-17 21:33:44 -0300

    Aula 17-01

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// Importa o framework Express, usado para criar o servidor e gerenciar rotas 
-import express from 'express';
+// Importa o framework Express, usado para criar o servidor e gerenciar rotas
+import express from "express";
 // Importa as rotas relacionadas a carros (carroRoutes.js)
-import carroRoutes from './routes/carroRoutes.js';
+import carroRoutes from "./routes/carroRoutes.js";
 
 // Cria uma instância do aplicativo Express
 const app = express();
@@ -10,10 +10,10 @@ const app = express();
 app.use(express.json());
 
 // Rotas
-app.use('/carros', carroRoutes);
+app.use("/carros", carroRoutes);
 
 // Inicializa o servidor para executar na porta 3000
 app.listen(3000, () => {
-    // Quando o servidor for iniciado, imprime no console que ele está rodando na porta 3000
-    console.log('Servidor Rodando com Sucesso na porta 3000');
+  // Quando o servidor for iniciado, imprime no console que ele está rodando na porta 3000
+  console.log("Servidor Rodando com Sucesso na porta 3000");
 });
